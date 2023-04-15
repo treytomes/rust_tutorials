0c04187d237b6f1453f020b89dcac89e471f9135
Trey Tomes <george.tomes@cityelectricsupply.com>
2023-04-15 08:04:17 -0500

    Fibonacci.

---
+++ 05-fibonacci/src/main.rs
@@ -0,0 +1,23 @@
+fn fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+fn main() {
+    // Increase the recursion limit to allow for larger values of n
+    // Note: this is not necessary for small values of n
+    //std::env::set_var("RUST_RECURSION_LIMIT", "10000");
+
+    let n = 1;
+
+    let result = fibonacci(n);
+    println!("Function: The {}th Fibonacci number is: {}", n, result);
+
+    let mac_result = fibonacci!(n);
+    println!("Macro: The {}th Fibonacci number is: {}", n, mac_result);
+}
