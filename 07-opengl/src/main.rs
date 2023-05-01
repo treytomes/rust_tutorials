03c2ec043d36a26b09794233d1795abf8acd2e07
Trey Tomes <george.tomes@cityelectricsupply.com>
2023-05-01 11:12:10 -0500

    OpenGL example.

---
+++ 07-opengl/src/main.rs
@@ -0,0 +1,33 @@
+extern crate gl;
+extern crate sdl2;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let window = video_subsystem
+        .window("Game", 800, 600)
+        .opengl()
+        .resizable()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let _gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => break 'main,
+                _ => {},
+            }
+        }
+
+        unsafe {
+            gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        
+        window.gl_swap_window();
+    }
+}
